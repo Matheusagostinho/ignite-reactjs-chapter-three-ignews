a2e03a68ae7e2adf471df80dff0b871dfd89d8b6
Matheus Agostinho <henrique.matheus28@gmail.com>
2021-07-24 18:45:52 -0300

    Finalizando a Aplicação

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { ActiveLink } from "../ActiveLink"
+import { SingInButton } from "../SignInButton"
+import styles from "./styles.module.scss"
+
+
+export function Header(){
+ 
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news"  />
+        <nav>
+          <ActiveLink href='/' activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href='/posts' activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+        <SingInButton/>
+      </div>
+    </header>
+  )
+}
